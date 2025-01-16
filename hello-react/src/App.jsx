a0d9c9c68235fa893c3b473122a38ace306f3f3e
Sai Bharath <saibharath1675@gmail.com>
2025-01-16 16:34:44 +0530

    Initial Commit

---
+++ hello-react/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import TaskCard from './TaskCard';
+
+function App() {
+  return (
+    <div className='flex flex-col items-center'>
+      <div>
+        <h1 className="font-bold text-4xl text-violet-600">Smarter Tasks</h1>
+      </div>
+      <div className='flex justify-center mt-8 gap-4'>
+        <div className='w-1/2 p-3 border border-black rounded-xl'>
+          <h1 className='text-3xl font-bold p-1 text-center text-gray-600'>Pending</h1>
+          <TaskCard title="Build the website with static content" dueDate="11th April" assigneeName="Ravi K"/>
+          <TaskCard title="Add a blog" dueDate="22nd March" assigneeName="Suresh M"/>
+        </div>
+        <div className='w-1/2 border p-3 rounded-xl border-black'>
+          <h1 className='text-3xl font-bold p-1 text-center text-gray-600'>Done</h1>
+          <TaskCard title="Design the mockup" completedAtDate="10th January" assigneeName="Ravi K"/>
+          <TaskCard title="Get the approval from principal" completedAtDate="15th January" assigneeName="Ravi K"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
---
+++ hello-react/src/TaskCard.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import './TaskCard.css';
+
+const TaskCard = (props) => {
+    let text = "";
+    let color = "";
+    if(props.completedAtDate) {
+      text = `Completed on: ${props.completedAtDate}`;
+      color = "text-green-500";
+    }
+    else {
+      text = `Due on: ${props.dueDate}`;
+      color = "text-red-500";
+    }
+    return(
+      <div className='TaskItem'>
+        <h2 className={`text-2xl font-medium ${color}`}>{props.title}</h2>
+        <p className='text-lg'>{text}</p>
+        <p className='text-lg'>Assignee: {props.assigneeName}</p>
+      </div>
+    );
+}
+  
+// Add PropTypes validation
+TaskCard.propTypes = {
+    title: PropTypes.string.isRequired,
+    completedAtDate: PropTypes.string,
+    dueDate: PropTypes.string,
+    assigneeName: PropTypes.string.isRequired
+};
+
+export default TaskCard;
