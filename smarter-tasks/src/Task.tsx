e7fb5ce17f9bcb35f6b88571b0f3a3aa91d1d1da
Sai Bharath <saibharath1675@gmail.com>
2025-01-19 22:31:27 +0530

    refactor: Converte to functional components using Hooks

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,23 +1,18 @@
-import React from "react";
 import "./TaskCard.css";
-interface TaskProp {
+interface TaskItem {
     title: string;
+    description: string;
+    dueDate: string;
 }
 
-class Task extends React.Component<TaskProp> {
-    render() {
-        return (
-            <div className="TaskItem shadow-md border border-slate-100">
-            <h2 className="text-base font-bold my-1">{this.props.title}</h2>
-            <p className="text-sm text-slate-500">
-            Due Date:
-            </p>
-            <p className="text-sm text-slate-500">
-            Description: 
-            </p>
-            </div>
-        )
-    }
-}
+const Task = (props: TaskItem) => {
+    return (
+      <div className="TaskItem shadow-md border border-slate-100">
+        <h2 className="text-base font-bold my-1">{props.title}</h2>
+        <p className="text-sm text-slate-500">{props.dueDate}</p>
+        <p className="text-sm text-slate-500">Description: {props.description}</p>
+      </div>
+    );
+  };
 
 export default Task;
--- smarter-tasks/src/TaskList.tsx
+++ smarter-tasks/src/TaskList.tsx
@@ -1,25 +1,20 @@
-import React from "react";
 import Task from "./Task";
 import { TaskItem } from "./types";
 
 interface Props {
     tasks: TaskItem[];
 }
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-interface State {
-  
-}
 
-class TaskList extends React.Component<Props, State> {
-    render() {
-        return (
-            <>
-              {this.props.tasks.map((task, idx) => (
-                <Task key={idx} title={task.title} />
-              ))}
-            </>
-        );
-    }
-}
+const TaskList = (props: Props) => {
+  const list = props.tasks.map((task, idx) => (
+    <Task
+      key={idx}
+      title={task.title}
+      description={task.description}
+      dueDate={task.dueDate}
+    />
+  ));
+  return <>{list}</>;
+};
 
 export default TaskList;
